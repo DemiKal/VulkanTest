587c4696eeb70cd850b0fdb26a73b77bdbae5d4e
DemiKal <damirkalender@hotmail.com>
2021-09-05 18:31:43 +0200

    fixed vertex/index flag bits

--- Vulkan Samples HPP/VulkanHPP.cpp
+++ Vulkan Samples HPP/VulkanHPP.cpp
@@ -2,6 +2,7 @@
 #define VKB_DEBUG 1
 #define VK_USE_PLATFORM_WIN32_KHR 1
 #define VMA_IMPLEMENTATION
+#define VKB_VALIDATION_LAYERS
 #include <tuple>
 #include "vk_mem_alloc.h"
 #include "VulkanHPP.h"
@@ -57,6 +58,7 @@ __pragma(warning(pop))
 		}                                                           \
 	} while (0)
 
+std::vector<const char*> get_optimal_validation_layers(const std::vector<vk::LayerProperties>&supported_instance_layers);
 
 VmaAllocator m_Allocator;
 
@@ -324,7 +326,7 @@ void VulkanHPP::InitAllocator(Context& context)
 }
 
 //template <typename T>
-void StageBuffer(Context& context, VertexBuffer& allocBuffer)
+void StageBuffer(Context& context, VertexBuffer& allocBuffer, vk::BufferUsageFlagBits usageFlags)
 {
 	const size_t bufferSize = allocBuffer.GetBufferSize();
 
@@ -350,7 +352,7 @@ void StageBuffer(Context& context, VertexBuffer& allocBuffer)
 	////////////////////////
 
 	//C++ style
-	auto usage = vk::BufferUsageFlagBits::eVertexBuffer | vk::BufferUsageFlagBits::eTransferDst;
+	auto usage =  usageFlags | vk::BufferUsageFlagBits::eTransferDst;
 	auto vkBuffer_CI = vk::BufferCreateInfo{ {}, bufferSize, usage };
 	VkBufferCreateInfo vertexBufferInfo = vkBuffer_CI; //convert!
 	//let the VMA library know that this data should be gpu native	
@@ -390,8 +392,8 @@ void StageBuffer(Context& context, VertexBuffer& allocBuffer)
 
 void VulkanHPP::InitVertices(Context& context)
 {
-	StageBuffer(context, triMesh.vertexBuffer);
-	StageBuffer(context, triMesh.indexBuffer);
+	StageBuffer(context, triMesh.vertexBuffer, vk::BufferUsageFlagBits::eVertexBuffer);
+	StageBuffer(context, triMesh.indexBuffer, vk::BufferUsageFlagBits::eIndexBuffer);
 }
 
 void VulkanHPP::InitWindow()
@@ -1387,4 +1389,62 @@ void VulkanHPP::InitLogger()
 	spdlog::set_default_logger(logger);
 	LOGI("Logger initialized");
 
+}
+
+bool validate_layers(const std::vector<const char*>& required,
+	const std::vector<vk::LayerProperties>& available)
+{
+	// inner find_if returns true if the layer was not found
+	// outer find_if returns iterator to the not found layer, if any
+	auto requiredButNotFoundIt = std::find_if(required.begin(),
+		required.end(),
+		[&available](auto layer) {
+			return std::find_if(available.begin(),
+				available.end(),
+				[&layer](auto const& lp) {
+					return strcmp(lp.layerName, layer) == 0;
+				}) == available.end();
+		});
+	if (requiredButNotFoundIt != required.end())
+	{
+		LOGE("Validation Layer {} not found", *requiredButNotFoundIt);
+	}
+	return (requiredButNotFoundIt == required.end());
+}
+
+
+std::vector<const char*> get_optimal_validation_layers(const std::vector<vk::LayerProperties>& supported_instance_layers)
+{
+	std::vector<std::vector<const char*>> validation_layer_priority_list =
+	{
+		// The preferred validation layer is "VK_LAYER_KHRONOS_validation"
+		{"VK_LAYER_KHRONOS_validation"},
+
+		// Otherwise we fallback to using the LunarG meta layer
+		{"VK_LAYER_LUNARG_standard_validation"},
+
+		// Otherwise we attempt to enable the individual layers that compose the LunarG meta layer since it doesn't exist
+		{
+			"VK_LAYER_GOOGLE_threading",
+			"VK_LAYER_LUNARG_parameter_validation",
+			"VK_LAYER_LUNARG_object_tracker",
+			"VK_LAYER_LUNARG_core_validation",
+			"VK_LAYER_GOOGLE_unique_objects",
+		},
+
+		// Otherwise as a last resort we fallback to attempting to enable the LunarG core layer
+		{"VK_LAYER_LUNARG_core_validation"} };
+
+	for (auto& validation_layers : validation_layer_priority_list)
+	{
+		if (validate_layers(validation_layers, supported_instance_layers))
+		{
+			return validation_layers;
+		}
+
+		LOGW("Couldn't enable validation layers (see log for error) - falling back");
+	}
+
+	// Else return nothing
+	return {};
 }
